4ba55a73edcc5540fad83c248a8001b9713ea995
msteele96 <marcsteele2014@gmail.com>
2021-03-29 18:06:30 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ActiveRecord::Base
+    has_many :rides
+    has_many :attractions, through: :rides
+
+    has_secure_password
+
+    def mood
+        if self.happiness > self.nausea
+            "happy"
+        else
+            "sad"
+        end
+    end
+
+    def public_attributes
+        {name: self.name, height: self.height, happiness: self.happiness, nausea: self.nausea, tickets: self.tickets}
+    end
+
 end
