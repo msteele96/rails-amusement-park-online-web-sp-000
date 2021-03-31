791b3b112d0481dcd3ccdffdaa25c1408276091b
msteele96 <marcsteele2014@gmail.com>
2021-03-31 00:02:07 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -10,6 +10,7 @@ class Ride < ActiveRecord::Base
             user.happiness = (user.happiness + attraction.happiness_rating)
             user.nausea = (user.nausea + attraction.nausea_rating)
             user.save
+            "Thanks for riding the #{attraction.name}!"
         elsif user.height >= attraction.min_height && user.tickets < attraction.tickets
             "Sorry. You do not have enough tickets to ride the #{attraction.name}."
         elsif user.height < attraction.min_height && user.tickets >= attraction.tickets
